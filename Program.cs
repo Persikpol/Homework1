e21787f43b035d23599f53826c6b7f1f3a2d0ce5
Elena <persik.popol@gmail.com>
2022-11-16 22:48:14 +0300

    Решила задачу 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 8
+Console.WriteLine("Введите число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i<=number)
+{
+    if (i%2 == 0)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
