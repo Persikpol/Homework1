cca230c6ebaea97b4d3863b25ac6e29d32dc6f68
Elena <persik.popol@gmail.com>
2022-11-16 22:47:14 +0300

    Решила задачу 6

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Task 6
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
