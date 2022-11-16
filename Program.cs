23bda57aaa94029bdf08fea5de8a2fc66f6a7292
Elena <persik.popol@gmail.com>
2022-11-16 22:42:59 +0300

    Решила задачу 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 2
+Console.WriteLine("Введите 1ое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ое число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1<number2)
+{
+    Console.WriteLine("Большее число " + number2);
+}
+else
+{
+    Console.WriteLine("Большее число " + number1);
+}
